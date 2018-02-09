18dafc06599fd0e3ff4946a89760035154129cc4
John Beech <johnbeech.github@mkv25.net>
2018-02-09 23:36:26 +0000

    Wire up the Objects page to /api/objects

--- src/components/pages/objects/Objects.js
+++ src/components/pages/objects/Objects.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
 
+const API = '/api/objects'
+
 class Objects extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  componentDidMount() {
+    update()
+
+    let self = this
+    function update() {
+      fetch(API)
+        .then(response => response.json())
+        .then(data => self.setState(data))
+        .catch(ex => console.error(ex))
+    }
+  }
+
+  componentWillUnmount() {
+  }
+
   render() {
+    function renderObjects(data) {
+
+      function mapObjects(key) {
+        const object = data[key]
+        return {
+          title: key,
+          rows: Object.keys(object)
+        }
+      }
+
+      return Object.keys(data)
+        .map(mapObjects)
+        .map(object => {
+          return (
+            <div key={object.title}>
+              <h2>{object.title}</h2>
+              {object.rows.map(row => (<p key={row}>{row}</p>))}
+            </div>
+          )
+        })
+    }
+
     return (
       <div className="Objects">
         <h1>Objects</h1>
+        {renderObjects(this.state)}
       </div>
-    );
+    )
   }
 }
 
