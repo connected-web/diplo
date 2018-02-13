1c8f50c5a2ee0a1c9f13994f14bae2ae3fce4e8c
John Beech <johnbeech.github@mkv25.net>
2018-02-13 06:44:30 +0000

    Support new data format in user interface

--- src/components/pages/objects/Objects.js
+++ src/components/pages/objects/Objects.js
@@ -7,20 +7,31 @@ class Objects extends Component {
     function renderObjects(data) {
       data = data || {}
 
-      function mapObjects(key) {
-        const object = data[key] || {}
+      function mapObjects(id) {
+        const object = data[id] || {}
         return {
-          title: key,
-          rows: Object.keys(object)
+          id: id,
+          properties: object.properties,
+          items: object.items
         }
+      }
+
+      function renderObject(data) {
+        return (
+          <div key={data.id}>
+            <h3>{data.id}</h3>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        )
       }
       return Object.keys(data)
         .map(mapObjects)
         .map(object => {
           return (
-            <div key={object.title}>
-              <h2>{object.title}</h2>
-              {object.rows.map(row => (<p key={row}>{row}</p>))}
+            <div key={object.id}>
+              <h2>{object.id}</h2>
+              <pre>{JSON.stringify(object.properties, null, 2)}</pre>
+              {object.items.map(renderObject)}
             </div>
           )
         })
