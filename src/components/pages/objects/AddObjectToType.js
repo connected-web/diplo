1ef8895271711789b255e759c543722e35bae866
John Beech <johnbeech.github@mkv25.net>
2018-02-18 08:36:04 +0000

    Create form based on object properties

--- src/components/pages/objects/AddObjectToType.js
+++ src/components/pages/objects/AddObjectToType.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Icon from '../../icon/Icon'
+import './Objects.css'
 
 class AddObjectToType extends Component {
   constructor() {
@@ -13,17 +12,34 @@ class AddObjectToType extends Component {
   render() {
     const model = this.props.model
     const objects = model.data.objects  || []
-    const url = this.props.match.url
+    // const url = this.props.match.url
     const objectPlural = this.props.match.params.object
     const objectSingular = objectPlural.slice(0, -1)
-    console.log('[AddObjectToType] objects', objects)
     const objectParent = objects.filter(n => n.id === objectSingular)[0] || {}
     const objectItems = objectParent.items || []
+    const objectProperties = objectParent.properties || {}
 
     let self = this
 
-    function onChange(ev) {
-      self.setState({value: ev.target.value})
+    function onIDChange(ev) {
+      const value = (ev.target.value || '').trim().toLowerCase().replace(/[_\s]/g, '-').replace(/[^A-z\d-]/g, '')
+      self.setState({value})
+    }
+
+    function renderPropertyInputs(objectProperties) {
+      return Object.keys(objectProperties).map(key => {
+        const inputType = objectProperties[key]
+        const keyName = `input-${key}-${inputType}`
+
+        function onPropertyChange(ev) {
+
+        }
+
+        return <pre className="ObjectProperty" key={keyName}>
+          <label htmlFor={keyName}>{key}</label>
+          <input name={keyName} onChange={onPropertyChange} /> : {inputType}
+        </pre>
+      })
     }
 
     return (
@@ -31,9 +47,13 @@ class AddObjectToType extends Component {
         <h1>
           <span>Add new {objectSingular}</span>
         </h1>
-        <pre>Enter new Object ID: <input name="object-id" onChange={onChange} /> : Will be used in file names, must be unique amongst other {objectPlural}</pre>
-        <pre>{self.state.value}</pre>
-        <pre>Exiting types: {objectItems.map(n => n.id).join(', ')}</pre>
+        <pre className="ObjectProperty">
+          <label for="object-id">New Object ID</label>
+          <input name="object-id" onChange={onIDChange} /> : This will be used in file names, and must be unique amongst other {objectSingular} types.
+        </pre>
+        <pre>Existing types: {objectItems.map(n => n.id).join(', ')}</pre>
+        {(self.state.value) ? <pre className="ObjectProperty"><label htmlFor='input-id-string'>id</label> {self.state.value}</pre> : ''}
+        {renderPropertyInputs(objectProperties)}
       </div>
     )
   }
--- src/components/pages/objects/CreateObjectType.js
+++ src/components/pages/objects/CreateObjectType.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Icon from '../../icon/Icon'
 
 class CreateObjectType extends Component {
   render() {
-    const model = this.props.model
+    // const model = this.props.model
 
     return (
       <div className="Objects">
