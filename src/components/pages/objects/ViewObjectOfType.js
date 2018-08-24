6a5421852d1aab234296ef42c1f48dfb1d33831f
John Beech <johnbeech.github@mkv25.net>
2018-08-24 07:05:55 +0100

    Display properties as separate lines

--- src/components/pages/objects/ViewObjectOfType.js
+++ src/components/pages/objects/ViewObjectOfType.js
@@ -21,13 +21,22 @@ class ViewObjectOfType extends Component {
     const objectId = this.props.match.params.id
     const objectParent = objects.filter(n => n.id === objectSingular)[0] || {}
     const objectItems = objectParent.items || []
-    const objectItem = objectItems.filter(n => n.id === objectId) || {}
+    const objectItem = objectItems.filter(n => n.id === objectId)[0] || {}
     const objectProperties = objectParent.properties || {}
+
+    function renderItemProperties(item) {
+      return Object.keys(objectProperties).map(key => {
+        const itemValue = item[key]
+        console.log(item, itemValue, key)
+        return <p className='ObjectProperty' key={`property-${key}`}><label>{key}</label><b>{itemValue}</b></p>
+      })
+    }
 
     return (
       <div className="Objects">
         <h1>View {objectSingular} : {objectId}</h1>
         <h3>Object item</h3>
+        {renderItemProperties(objectItem)}
         <pre>{JSON.stringify(objectItem, null, 2)}</pre>
         <h3>Object properties</h3>
         <pre>{JSON.stringify(objectProperties, null, 2)}</pre>
