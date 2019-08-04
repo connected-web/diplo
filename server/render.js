9348536b2f5c79f6eb8bbd365dbe2db99ab8ed38
John Beech <johnbeech.github@mkv25.net>
2019-08-04 19:09:13 +0100

    Make rendering better

--- server/render.js
+++ server/render.js
@@ -1,10 +1,17 @@
 const path = require('path')
 const Nightmare = require('nightmare')
 
-function render(url, filename) {
-  return Nightmare()
+function render(url, filename, clipRectangle) {
+  return Nightmare({
+      frame: false,
+      useContentSize: true,
+      transparent: true,
+    })
+    .viewport(clipRectangle.width, clipRectangle.height)
     .goto(url)
-    .screenshot(filename)
+    .wait('body')
+    .screenshot(filename, clipRectangle)
+    .end()
 }
 
 function configure(model) {
@@ -12,13 +19,17 @@ function configure(model) {
   const serverPath = `http://localhost:${port}`
   async function handleRequest(req, res) {
     const { typeId, itemId, templateId } = req.params
+    const template = model.templates.filter(template => template.id === templateId)[0] || {}
+    const properties = template.properties || { size: { width: 100, height: 100 } }
+    const clipRectangle = { x: 0, y: 0, width: properties.size.width, height: properties.size.height }
+
     const context = `Trying to render image for ${templateId} using ${typeId}, ${itemId}`
     const assetPath = path.join(workingPath, 'assets', `${typeId}-${itemId}-${templateId}.png`)
-    console.log(`[RENDER] ${context}`)
+    console.log(`[RENDER] ${context}`, 'Size', properties.size)
 
     try {
       const presentableUrl = `${serverPath}/api/present/${typeId}/${itemId}/${templateId}`
-      await render(presentableUrl, assetPath)
+      await render(presentableUrl, assetPath, clipRectangle)
       res.sendFile(assetPath)
     }
     catch (ex) {
